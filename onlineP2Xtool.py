4074ce8cb7073c8826a2f251b7b3b7b116c471a3
tomaswierzba <103575861+tomaswierzba@users.noreply.github.com>
2022-08-11 16:29:16 +0200

    Update onlineP2Xtool.py

--- onlineP2Xtool.py
+++ onlineP2Xtool.py
@@ -106,7 +106,7 @@ for t in range (1,len(cf)):
 year=np.linspace(0, lifetime,lifetime+1)
 chart_data2 = pd.DataFrame({'Year':year,'Non-disc Cash Flows in Million €':cf})
 d = alt.Chart(chart_data2).mark_bar().encode(
-     x='Year:O',y='Non-disc Cash Flows in Million €:Q',color=alt.value('#ffe300')).configure(background='#193047').properties(
+     x='Year:O',y='Non-disc Cash Flows in Million €:Q',color=alt.value('#ffe300')).configure(background='white').properties(
     title='Non-discounted Cash Flows'
 )
 d.configure_title(
@@ -129,7 +129,7 @@ for i in range(1,len(cf)):
 
 chart_data3 = pd.DataFrame({'Year':year,"Acc Disc Cash Flows in Million €":NPV})
 c = alt.Chart(chart_data3).mark_bar().encode(
-     x='Year:O',y="Acc Disc Cash Flows in Million €",color=alt.value('#ffe300')).configure(background='#193047').properties(
+     x='Year:O',y="Acc Disc Cash Flows in Million €",color=alt.value('#ffe300')).configure(background='white').properties(
     title='Accumulated Discounted Cash Flows'
 )
 d.configure_title(
