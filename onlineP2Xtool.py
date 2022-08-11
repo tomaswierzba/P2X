78c40d75a676e1e35ba10311b1dadc85b2c13136
tomaswierzba <103575861+tomaswierzba@users.noreply.github.com>
2022-08-11 18:20:31 +0200

    Update onlineP2Xtool.py

--- onlineP2Xtool.py
+++ onlineP2Xtool.py
@@ -110,6 +110,7 @@ d = alt.Chart(chart_data2).mark_bar().encode(
     title='NEXP2X Business-Case Tool',width= 600, height= 400
 ).configure_title(
     fontSize=30,
+    fontWeight='bold'
     font='monospace',
     anchor='middle',
     color='#f0f2f6'
