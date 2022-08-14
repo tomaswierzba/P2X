9b83e929a93c11fc0b24feffad5a0f25a0207fb5
tomaswierzba <103575861+tomaswierzba@users.noreply.github.com>
2022-08-14 17:00:36 +0200

    Update onlineP2Xtool.py

--- onlineP2Xtool.py
+++ onlineP2Xtool.py
@@ -176,7 +176,7 @@ LCoH2 = round(LCoH,1)
 #new_title7 = '<p style="font-size:45px;font-weight:700;color:black;text-align:center;">Results</p>'
 #st.write(new_title7, unsafe_allow_html=True)
 st.write(""" # Results """)
-col1, col2, col3 = st.columns(4)
+col1, col2, col3,col4 = st.columns(4)
 col1.metric("Payback time:", '%s' % (a101))
 col2.metric("NPV", "%s M€"  % (npv2))
 col3.metric("IRR", "%s %" % (IRR2))
