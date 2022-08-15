a4180207ffabd61adebb7fc9a828a6d4c3fb91cd
tomaswierzba <103575861+tomaswierzba@users.noreply.github.com>
2022-08-15 11:17:50 +0200

    Update onlineP2Xtool.py

--- onlineP2Xtool.py
+++ onlineP2Xtool.py
@@ -235,7 +235,8 @@ st.altair_chart(d, use_container_width=True)
 brush = alt.selection_interval()
 chart_data3 = pd.DataFrame({'Year':year,"Acc Disc Cash Flows in Million €":NPV})
 c = alt.Chart(chart_data3).mark_bar().encode(
-     x='Year:O',y="Acc Disc Cash Flows in Million €",color=alt.value('#ffe300')).properties(
+     x='Year:O',y="Acc Disc Cash Flows in Million €", color=alt.condition(brush, alt.value('#ffe300'), alt.value('lightgray'))
+).properties(
     title='Accumulated Discounted Cash Flows'
 ).configure_title(
     fontSize=25,
