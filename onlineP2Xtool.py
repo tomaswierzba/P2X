2e9522917eb6638af220b873da20be16832bc4d8
tomaswierzba <103575861+tomaswierzba@users.noreply.github.com>
2022-08-15 14:23:59 +0200

    Update onlineP2Xtool.py

--- onlineP2Xtool.py
+++ onlineP2Xtool.py
@@ -237,7 +237,7 @@ d = alt.Chart(chart_data2).mark_bar().encode(
 st.altair_chart(d, use_container_width=True) 
 
 brush = alt.selection_interval()
-chart_data3 = pd.DataFrame({'Year':year,"Acc Disc Cash Flows in Million €":NPV})
+chart_data3 = pd.DataFrame({'Year':year,"Acc Disc Cash Flows in Million €":NPV},"Payback Time":a1010)
 c = alt.Chart(chart_data3).mark_bar().encode(
      x='Year:O',y="Acc Disc Cash Flows in Million €", color=alt.value('#ffe300') ).interactive().properties(
     title='Accumulated Discounted Cash Flows',width= 600, height= 400
@@ -248,8 +248,9 @@ c = alt.Chart(chart_data3).mark_bar().encode(
     color='#f0f2f6'
 ).configure_axis(titleColor='#f0f2f6',labelColor='#f0f2f6',labelAngle=0,labelFontSize=15,titleFontSize=15, gridColor='black') #.add_selection(brush)
 
+
 rule = alt.Chart(chart_data3).mark_line(color='green').encode(
-    x=a1010
+    x='Payback Time:Q'
 )
 
 st.altair_chart(c+rule, use_container_width=True)
