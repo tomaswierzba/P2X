7713a94dae8c781ddef522975684bbed5d1955e3
tomaswierzba <103575861+tomaswierzba@users.noreply.github.com>
2022-08-15 11:52:53 +0200

    Update onlineP2Xtool.py

--- onlineP2Xtool.py
+++ onlineP2Xtool.py
@@ -218,19 +218,19 @@ c2 = base.mark_text(radiusOffset=20).encode(text="labels:N")
 
 c1 + c2
 
-brush = alt.selection_interval()
+brush2 = alt.selection_interval()
 st.write(" # Cash flow plots")
 year=np.linspace(0, lifetime,lifetime+1)
 chart_data2 = pd.DataFrame({'Year':year,'Non-discounted Cash Flows in Million €':cf})
 d = alt.Chart(chart_data2).mark_bar().encode(
-     x='Year:O',y='Non-discounted Cash Flows in Million €:Q',color=alt.condition(brush, alt.value('#ffe300'), alt.value('lightgray')).interactive().properties(
+     x='Year:O',y='Non-discounted Cash Flows in Million €:Q',color=alt.condition(brush2, alt.value('#ffe300'), alt.value('lightgray')).interactive().properties(
     title='Non-discounted Cash Flows',width= 600, height= 400
 ).configure_title(
     fontSize=25,
     fontWeight=900,
     anchor='middle',
     color='#f0f2f6'
-).configure_axis(titleColor='#f0f2f6',labelColor='#f0f2f6',labelAngle=0,labelFontSize=15,titleFontSize=15, gridColor='black') #.configure(background='white').add_selection(brush)
+).configure_axis(titleColor='#f0f2f6',labelColor='#f0f2f6',labelAngle=0,labelFontSize=15,titleFontSize=15, gridColor='black').add_selection(brush2) #.configure(background='white')
 
 st.altair_chart(d, use_container_width=True) 
 
