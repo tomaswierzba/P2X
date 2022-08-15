c3b80be6082014207cc6cbee8d478530fbc89a2e
tomaswierzba <103575861+tomaswierzba@users.noreply.github.com>
2022-08-15 14:20:18 +0200

    Update onlineP2Xtool.py

--- onlineP2Xtool.py
+++ onlineP2Xtool.py
@@ -248,7 +248,7 @@ c = alt.Chart(chart_data3).mark_bar().encode(
     color='#f0f2f6'
 ).configure_axis(titleColor='#f0f2f6',labelColor='#f0f2f6',labelAngle=0,labelFontSize=15,titleFontSize=15, gridColor='black') #.add_selection(brush)
 
-rule = alt.Chart(chart_data3).mark_rule(color='green').encode(
+rule = alt.Chart(chart_data3).mark_line(color='green').encode(
     x=a1010
 )
 
