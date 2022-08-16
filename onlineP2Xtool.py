7ea2c8c0087029de562563d21002c2a4c7d83a9f
tomaswierzba <103575861+tomaswierzba@users.noreply.github.com>
2022-08-16 12:54:27 +0200

    Update onlineP2Xtool.py

--- onlineP2Xtool.py
+++ onlineP2Xtool.py
@@ -285,6 +285,6 @@ else:
     
 
 st.write(" #  What's your next action towards 100% renewables?")
-#new_title100 = '<p style="font-size:20px;font-weight:600;color:#ffe300> Let''s create more value together, send us an e-mail to <a href = "mailto: abc@example.com">info@hybridgreentech.com</a></p>'
-#st.write(new_title100, unsafe_allow_html=True)
+new_title100 = '<p style="font-size:20px;font-weight:600;color:#ffe300> Lets create more value together, send us an e-mail to <a href = "mailto: abc@example.com">info@hybridgreentech.com</a></p>'
+st.write(new_title100, unsafe_allow_html=True)
 st.write("Let's create more value together, send us an e-mail to info@hybridgreentech.com")
