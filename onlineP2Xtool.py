3d6cbbeca86491a0192e652b975a77c16265cc4b
tomaswierzba <103575861+tomaswierzba@users.noreply.github.com>
2022-08-17 14:46:48 +0200

    Update onlineP2Xtool.py

--- onlineP2Xtool.py
+++ onlineP2Xtool.py
@@ -220,7 +220,7 @@ base = alt.Chart(source).encode(
 
 c1 = base.mark_arc(innerRadius=20)
 
-c2 = base.mark_text(radiusOffset=40).encode(text="labels:N")
+c2 = base.mark_text(radiusOffset=45).encode(text="labels:N")
 #c2 = base.mark_text(radiusOffset=10, align='left',
  #   baseline='middle').encode(text="Values:Q", color=alt.value('white'))
 
